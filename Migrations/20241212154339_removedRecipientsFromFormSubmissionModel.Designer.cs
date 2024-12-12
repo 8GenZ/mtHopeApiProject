435662cc80e1b5250ee954409764d9d49c452e47
Jonathan Chavez <jonachav6414@gmail.com>
2024-12-12 11:30:34 -0500

    added new migration

--- Migrations/20241212154339_removedRecipientsFromFormSubmissionModel.Designer.cs
+++ Migrations/20241212154339_removedRecipientsFromFormSubmissionModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mtHopeApiProject.Data;
 
@@ -11,9 +12,11 @@ using mtHopeApiProject.Data;
 namespace mtHopeApiProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241212154339_removedRecipientsFromFormSubmissionModel")]
+    partial class removedRecipientsFromFormSubmissionModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241212154339_removedRecipientsFromFormSubmissionModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mtHopeApiProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class removedRecipientsFromFormSubmissionModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FormSubmissions_Recipients_RecipientId",
+                table: "FormSubmissions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FormSubmissions_RecipientId",
+                table: "FormSubmissions");
+
+            migrationBuilder.DropColumn(
+                name: "RecipientId",
+                table: "FormSubmissions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RecipientId",
+                table: "FormSubmissions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormSubmissions_RecipientId",
+                table: "FormSubmissions",
+                column: "RecipientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FormSubmissions_Recipients_RecipientId",
+                table: "FormSubmissions",
+                column: "RecipientId",
+                principalTable: "Recipients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
